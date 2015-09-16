6b7117efbb9564a068a8f42d8e0b9ea4ee1f7d05
Viktor Likin <viktor.likin@gmail.com>
2015-09-16 11:12:52 +0300

    The parallax videos are finished.

--- src/ui/lib/controller.js
+++ src/ui/lib/controller.js
@@ -27,6 +27,30 @@
               }, 500 * index, el);
           });
         }
+
+        var large_window = angular.element('.large-window');
+        if (wScroll > large_window.offset().top - large_window.height()) {
+          var shift = wScroll - large_window.offset().top;
+          var shift_str = 'center -' + parseInt(shift) + 'px';
+          large_window.css('background-position', shift_str);
+
+          var window_tint = large_window.find('.window-tint');
+          var promo_text = window_tint.find('.promo-text');
+          var opacity = (1 - (large_window.offset().top - wScroll) / large_window.height());
+          opacity = opacity <= 1 || opacity >= 0 ? opacity : 1;
+          promo_text.css('opacity', opacity); 
+        }
+
+        var blog_posts = angular.element('.blog-posts');
+        var post_1 = blog_posts.find('.post-1');
+        var post_2 = blog_posts.find('.post-2');
+        var post_3 = blog_posts.find('.post-3');
+        if (wScroll > blog_posts.offset().top - window.height()) {
+          var offset = wScroll - blog_posts.offset().top + window.height() - 200;
+          offset = Math.abs(offset);
+          post_1.css('transform', 'translate(' + offset + 'px, 20px)')
+          post_3.css('transform', 'translate(' + -offset + 'px, 20px)')
+        }
     }
   );
 })
